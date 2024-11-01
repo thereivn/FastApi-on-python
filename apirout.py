f82c53d86e736492130662aa0f67df419cb04d9b
Nikita Zim <n_zim_r@mail.ru>
2024-11-01 14:09:32 +0500

    apirout updated 14:09

--- apirout.py
+++ apirout.py
@@ -8,15 +8,14 @@ todo_list = []
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {
-        "Put " and todo and " with message": "Todo added successfully."
+        "message": "Todo added successfully.",
     }
 
 @todo_router.get("/todo")
 async def retrieve_todo() -> dict:
     return {
-        "Get " and todo_list and " from todos": todo_list
+        "todos": todo_list
     }
-
 
 @todo_router.get("/todo/{todo_id}")
 async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
@@ -46,3 +45,16 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
     # return {
     # "message": "Todo with supplied ID doesn't exist."
     # }   
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index] 
+        if todo.id == todo_id:
+            todo_list.pop(index) 
+            return { "message": "Todo deleted successfully."}
+    raise HTTPException(status_code=404, detail="Todo with supplied ID doesn't exist.")
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
