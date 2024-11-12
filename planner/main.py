32477f7547983322d8cda6aa6ecc89cbe1af7821
Nikita Zim <n_zim_r@mail.ru>
2024-11-12 21:57:47 +0500

    Updated main.py and final ver. routes/events.py

--- planner/main.py
+++ planner/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 import uvicorn
 app = FastAPI()
 # Register routes
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
